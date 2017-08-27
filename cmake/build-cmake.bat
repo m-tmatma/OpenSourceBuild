164e3ad80d869a4ea1a436a32e01e29b6dcb318b
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-08-27 20:57:03 +0900

    set configuration and build package

--- cmake/build-cmake.bat
+++ cmake/build-cmake.bat
@@ -1,12 +1,14 @@
 set INITDIR=%CD%
+set ARCHITECTURE=x86
+set CONFIGURATION=Release
 
 @echo on
 if "%VS150COMNTOOLS%" == "" (
-	call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+	call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %ARCHITECTURE%
 )
 
 set SRC_CMAKE=%INITDIR%\cmake
-set OUTDIR_CMAKE=%INITDIR%\build-cmake
+set OUTDIR_CMAKE=%INITDIR%\build-cmake-%ARCHITECTURE%-%CONFIGURATION%
 
 if not exist "%SRC_CMAKE%" (
 	git clone https://gitlab.kitware.com/cmake/cmake.git %SRC_CMAKE%
@@ -17,8 +19,9 @@ if not exist %OUTDIR_CMAKE%      mkdir %OUTDIR_CMAKE%
 
 cd /d %OUTDIR_CMAKE%
 
-cmake -G Ninja %SRC_CMAKE%  || goto error-end
+cmake -G Ninja -D CMAKE_BUILD_TYPE=%CONFIGURATION% %SRC_CMAKE%  || goto error-end
 ninja -v                    || goto error-end
+ninja -v package            || goto error-end
 
 cd /d %INITDIR%\
 exit /b 0
